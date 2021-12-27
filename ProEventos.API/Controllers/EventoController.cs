15cd5670c671d495b47106064c79aa4d2d217a26
Allyson Andrade <allyson.andrade@sotran.com.br>
2021-12-26 21:44:39 -0300

    Add project files.

---
+++ ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
+using ProEventos.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        private readonly DataContext context;
+
+        public EventoController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return context.Eventos;
+        }
+
+        [HttpGet("{id}")]
+        public Evento GetById(int id)
+        {
+            return context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
+        }
+
+
+    }
+}
